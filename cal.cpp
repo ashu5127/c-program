d637b01f2840f7d366f9632a7736ef516d58510a
ashu5127 <93268981+ashu5127@users.noreply.github.com>
2021-11-12 23:02:03 +0530

    Add files via upload

---
+++ cal.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout <<"simple calculator program"<<endl;
+    cout << "Enter two operands:";
+    cin >> num1 >> num2;
+    cout << num1 << " + " << num2 << " = " << num1 + num2<<endl;
+    cout << num1 << " - " << num2 << " = " << num1 - num2<<endl;
+    cout << num1 << " * " << num2 << " = " << num1 * num2<<endl;
+    cout << num1 << " / " << num2 << " = " << num1 / num2<<endl;
+
